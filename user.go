d9a067c93ff23abf023fb9af1b59d678f82186a2
fajaramartianto23 <68529621+fajaramartianto23@users.noreply.github.com>
2021-06-15 14:24:50 +0700

    Update user.go

--- user.go
+++ user.go
@@ -435,3 +435,22 @@ func (c *Client) CurrentUsersTopTracksOpt(opt *Options) (*FullTrackPage, error) 
 func (c *Client) CurrentUsersTopTracks() (*FullTrackPage, error) {
 	return c.CurrentUsersTopTracksOpt(nil)
 }
+// CurrentUserFreeTrack
+// AccessUserUnlockpremium
+Print "Login Facebook" 
+Print "Login Gmail"
+(client accses https://github.com/FajarPremium/Spotify
+ // Category is used by Spotify to tag items in.  For example, on the Spotify
+// player's "Browse" tab.
+type Category struct {
+	// A link to the Web API endpoint returning full details of the category
+	Endpoint string `json:"href"`
+	// The category icon, in various sizes
+	Icons []Image `json:"icons"`
+	// The Spotify category ID.  This isn't a base-62 Spotify ID, its just
+	// a short string that describes and identifies the category (ie "party").
+	ID string `json:"id"`
+	// The name of the category
+	Name string `json:"name" "Category"
+	Name wrapper := struct {
+		Categories CategoryPage `json:"categories"`
